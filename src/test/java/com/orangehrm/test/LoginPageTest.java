e98b87fee3aa4805e87d1606b31b94004f8304ad
hverma22 <hitendraverma22@gmail.com>
2025-03-03 00:41:37 +0530

    Created Jenkins file for pipeline job

--- src/test/java/com/orangehrm/test/LoginPageTest.java
+++ src/test/java/com/orangehrm/test/LoginPageTest.java
@@ -42,7 +42,7 @@ public class LoginPageTest extends BaseClass {
 		System.out.println("Running testMethod2 on thread: " + Thread.currentThread().getId());
 		ExtentManager.logStep("Navigating to Login Page entering username and password");
 		loginPage.login(username, password);
-		String expectedErrorMessage = "Invalid credentials";
+		String expectedErrorMessage = "Invalid credentials1";
 		Assert.assertTrue(loginPage.verifyErrorMessage(expectedErrorMessage),"Test Failed: Invalid error message");
 		ExtentManager.logStep("Validation Successful");
 	}
